99736c328bba1a0b39bdc3469262772401409f66
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-01-29 21:55:03 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/README.md
+++ 프로그래머스/lv3/43163. 단어 변환/README.md
@@ -1,6 +1,6 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
 
 ### 성능 요약
 
--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,25 +1,23 @@
 from collections import deque
 def solution(begin, target, words):
+    visited = [False] * len(words)
+    answer = 0
     q = deque()
     q.append([begin,0])
-    answer = 0
-    visited =  [0]*len(words)
     while q:
-        word,cnt = q.popleft()
-        if word == target:
+        now,cnt = q.popleft()
+        if now == target:
             answer = cnt
             break
         for i in range(len(words)):
-            temp_cnt = 0
-            if not visited[i]:
-                for j in range(len(word)):
-                    if word[j] != words[i][j]:
-                        temp_cnt += 1
-                if temp_cnt == 1:
-                    visited[i] = 1
-                    q.append([words[i],cnt + 1])
+            trans_cnt = 0
+            if visited[i] == False:
+                for j in range(len(now)):
+                    if now[j] != words[i][j]:
+                        trans_cnt += 1
+                if trans_cnt == 1:
+                    q.append([words[i],cnt+1])
+                    visited[i] = True
     return answer
                 
-            
-        
-        
+    
