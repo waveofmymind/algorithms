f043663d2640627ba3ec89adf2833dbe8eacadf0
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-02-13 15:14:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.28 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    size = len(nums)//2
-    s = set(nums)
-    if len(s) > size:
-        return size
-    else:
-        return len(s)
+    return min(len(set(nums)),len(nums)//2)
