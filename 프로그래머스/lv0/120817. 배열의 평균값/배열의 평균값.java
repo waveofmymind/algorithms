3129a926a45af32481614b24068d7294c91ae282
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-02-23 10:18:25 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum/numbers.length;
+    }
+}
