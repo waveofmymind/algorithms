850ce5241c16e4a41db69ae814b8788f814a5fa8
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-03-13 23:26:49 +0900

    [level 2] Title: 구명보트, Time: 14.99 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,22 +1,27 @@
 from collections import deque
+
 def solution(people, limit):
-    cnt = 0
+    answer = 0
     people.sort()
-    lt = 0
-    rt = len(people)-1
-    while lt<=rt:
-        cnt += 1
-        if (people[lt] + people[rt] <= limit):
-            lt += 1
-            rt -= 1
+    kg = deque(people)
+
+    while kg:
+
+        if len(kg) >=2:
+            if kg[0] + kg[-1] <= limit:
+                kg.popleft()
+                kg.pop()
+                answer +=1
+            elif kg[0] + kg[-1] > limit:
+                kg.pop()
+                answer+=1
         else:
-            rt -= 1
-    return cnt
-    
-    
-        
-        
-            
-            
-            
-        
+            kg.pop()
+            answer +=1
+
+
+
+
+
+
+    return answer
