acd10442857f160cc7e00718b9184c3a11c531c3
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-03-21 17:21:12 +0900

    [level 2] Title: 올바른 괄호, Time: 9.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -3,17 +3,20 @@ from collections import deque
 def solution(s):
     stack = []
     for i in s:
-        if i == '(':
+        if not stack:
             stack.append(i)
         else:
-            if stack and stack[-1] == '(':
-                stack.pop()
+            if i == ')':
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    return false
             else:
-                return False
-            
-    if stack:
-        return False
-    else:
+                stack.append(i)
+    if not stack:
         return True
+    else:
+        return False
+                    
                     
         
